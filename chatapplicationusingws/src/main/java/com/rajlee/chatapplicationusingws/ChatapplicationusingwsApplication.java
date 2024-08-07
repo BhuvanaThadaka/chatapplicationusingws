6df2ea942a8717d628052a7e95c3c362da7c6244
Bhuvana <bhuvanathadaka19@gmail.com>
2024-08-07 11:46:27 +0530

    Initial commit

---
+++ chatapplicationusingws/src/main/java/com/rajlee/chatapplicationusingws/ChatapplicationusingwsApplication.java
@@ -0,0 +1,13 @@
+package com.rajlee.chatapplicationusingws;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChatapplicationusingwsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChatapplicationusingwsApplication.class, args);
+	}
+
+}
---
+++ chatapplicationusingws/src/test/java/com/rajlee/chatapplicationusingws/ChatapplicationusingwsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rajlee.chatapplicationusingws;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatapplicationusingwsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
